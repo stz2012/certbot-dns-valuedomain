1a4a265ea62ec4505465a3a181109bef42c7e0aa
stz2012 <tslroom@hotmail.com>
2026-06-07 00:16:01 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as fp:
 
 setup(
     name='certbot-dns-valuedomain',
-    version='1.0.0',
+    version='1.1.0',
     author='chrono-meter@gmx.net',
     author_email='chrono-meter@gmx.net',
     description='Certbot plugin for authentication using value-domain',
@@ -19,7 +19,6 @@ setup(
     install_requires=[
         'certbot',
         'requests',
-        'beautifulsoup4',
         # 'dnspython',
     ],
     entry_points={
